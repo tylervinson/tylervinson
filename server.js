6a154882ce9186177dc32bec74edd201fbceb3f5
tylervinson <126958994+tylervinson@users.noreply.github.com>
2025-06-04 16:55:19 -0700

    Fix server file reading bug

--- server.js
+++ server.js
@@ -5,18 +5,26 @@ const path = require('path');
 const server = http.createServer((req, res) => {
   let filePath = '.' + req.url;
   if (filePath === './') filePath = './cq3.html';
-  
+
   const extname = path.extname(filePath);
   let contentType = 'text/html';
-  
+
   switch (extname) {
-    case '.js': contentType = 'text/javascript'; break;
-    case '.css': contentType = 'text/css'; break;
-    case '.json': contentType = 'application/json'; break;
-    case '.xlsx': contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'; break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.xlsx':
+      contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+      break;
   }
-  
-  fs.readFile(filePath, (err, content) ersw3gtersx  `dsacsaxcasdcasdfsadf`=> {
+
+  fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code === 'ENOENT') {
         res.writeHead(404);
@@ -33,4 +41,6 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = 8000;
-server.listen(PORT, '127.0.0.1', () => console.log(`Server running at http://localhost:${PORT}`));
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
